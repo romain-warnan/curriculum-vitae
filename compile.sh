09d28e9bf24dc37064b4af99783e85f4ed70634b
Romain Warnan <romain.warnan@insee.fr>
2019-01-14 13:58:35 +0100

    Programme de compilation plus élaboré

--- compile.sh
+++ compile.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-cv_style=${1:-'classic'}
-cv_color=${2:-'burgundy'}
+usage () {
+	echo "Usage:"
+	echo "   ./compile.sh [-s style] [-c color]"
+	echo
+	echo "Available styles:"
+	echo "   'casual', 'classic' (default), 'banking', 'oldstyle' and 'fancy'"
+	echo "Available colors:"
+	echo "   'black', 'blue', 'burgundy' (default), 'green', 'grey', 'orange', 'purple' and 'red'"
+	echo
+	echo "Examples:"
+	echo "   ./compile.sh -s casual -c green"
+	echo "   ./compile.sh -s fancy"
+	echo "   ./compile.sh -c purple"
+	echo "   ./compile.sh"
+	exit
+}
+
+
+while getopts ":c:s:" options; do
+  case ${options} in
+    s) cv_style="$OPTARG"
+    ;;
+    c) cv_color="$OPTARG"
+    ;;
+    \?) usage
+    ;;
+  esac
+done
+
+cv_style=${cv_style:-'classic'}
+cv_color=${cv_color:-'burgundy'}
+
+printf "Generate PDF style '%s', color '%s'\n" "$cv_style" "$cv_color"
+
 cv_filename=cv-${cv_style}-${cv_color}.tex
 
 sed "s/moderncvstyle{classic}/moderncvstyle{${cv_style}}/g" cv.tex > ${cv_filename}
